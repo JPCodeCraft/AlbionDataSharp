fc10be633e1122e4f38e110512de474c2ae2ad1d
Augusto Ayub <augustoayub85@gmail.com>
2023-09-20 23:22:33 -0300

    changed to Host.CreateDefaultBuilder

--- AlbionDataSharp/Program.cs
+++ AlbionDataSharp/Program.cs
@@ -1,9 +1,12 @@
-﻿using Serilog;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Configuration;
+﻿using AlbionDataSharp.Config;
 using AlbionDataSharp.Network;
-using AlbionDataSharp.Config;
+using AlbionDataSharp.State;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace AlbionDataSharp
 {
@@ -12,18 +15,28 @@ namespace AlbionDataSharp
 
         private static void Main(string[] args)
         {
-            HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
+            var builder = Host.CreateDefaultBuilder(args)
+                //AUTOFAC CONFIG
+                .UseServiceProviderFactory(new AutofacServiceProviderFactory())
+                .ConfigureContainer<ContainerBuilder>(builder =>
+                {
+                    builder.RegisterType<PlayerStatus>().SingleInstance();
+                })
+                //SERVICES CONFIG
+                .ConfigureServices((hostContext, services) =>
+                {
+                    services.AddHostedService<NetworkListener>();
+                    services.AddSerilog(config =>
+                    {
+                        config.ReadFrom.Configuration(hostContext.Configuration);
 
-            builder.Services.AddHostedService<NetworkListener>();
-            builder.Services.AddSerilog(config =>
-            {
-                config.ReadFrom.Configuration(builder.Configuration);
+                    });
+                });
 
-            });
             IHost host = builder.Build();
 
             ConfigurationHelper.Initialize(host.Services.GetRequiredService<IConfiguration>());
-            
+
             host.Run();
         }
 
