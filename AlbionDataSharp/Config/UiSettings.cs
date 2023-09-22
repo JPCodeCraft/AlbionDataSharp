81748c2db733146935521ce7bef5b8b80c393912
JP Codecraft <jpcode@craft.com>
2023-09-22 17:06:36 -0300

    UI is ok-ish

--- AlbionDataSharp/Config/UiSettings.cs
+++ AlbionDataSharp/Config/UiSettings.cs
@@ -2,16 +2,6 @@
 {
     internal class UiSettings
     {
-        public required int TitleLine { get; set; }
-        public required int ServerLine { get; set; }
-        public required int NameLine { get; set; }
-        public required int LocationLine { get; set; }
-        public required int OffersLine { get; set; }
-        public required int RequestsLine { get; set; }
-        public required int HistoriesMonthLine { get; set; }
-        public required int HistoriesWeekLine { get; set; }
-        public required int HistoriesDayLine { get; set; }
-        public required int StateStartLine { get; set; }
-        public required int StateLineCount { get; set; }
+        public required int MaxLogEntries { get; set; }
     }
 }
