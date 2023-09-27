bebf0b246e902534edd971cf436f4322676f71c1
Augusto Ayub <augustoayub85@gmail.com>
2023-09-27 17:47:46 -0300

    Events to update console UI, closes #4

---
+++ AlbionDataSharp/Network/Events/GoldPriceUploadEventArgs.cs
@@ -0,0 +1,16 @@
+﻿using AlbionData.Models;
+using AlbionDataSharp.Config;
+
+namespace AlbionDataSharp.Network.Events
+{
+    public class GoldPriceUploadEventArgs : EventArgs
+    {
+        public GoldPriceUpload GoldPriceUpload { get; set; }
+        public ServerInfo Server { get; set; }
+        public GoldPriceUploadEventArgs(GoldPriceUpload goldPriceUpload, ServerInfo serverInfo)
+        {
+            GoldPriceUpload = goldPriceUpload;
+            Server = serverInfo;
+        }
+    }
+}
---
+++ AlbionDataSharp/State/Events/PlayerStateEventArgs.cs
@@ -0,0 +1,18 @@
+﻿using AlbionData.Models;
+using AlbionDataSharp.Config;
+
+namespace AlbionDataSharp.Network.Events
+{
+    public class PlayerStateEventArgs : EventArgs
+    {
+        public Location Location { get; set; }
+        public string Name { get; set; }
+        public AlbionServer AlbionServer { get; set; }
+        public PlayerStateEventArgs(Location location, string name, AlbionServer albionServer)
+        {
+            Location = location;
+            Name = name;
+            AlbionServer = albionServer;
+        }
+    }
+}
