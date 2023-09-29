6e5e13d56e8951776c4551c1bd5a492abfeed7b3
JP Codecraft <jpcode@craft.com>
2023-09-28 22:22:34 -0300

    servers check before connecting closes #5

--- AlbionDataSharp/Program.cs
+++ AlbionDataSharp/Program.cs
@@ -18,13 +18,14 @@ namespace AlbionDataSharp
 
             HostApplicationBuilder builder = Host.CreateApplicationBuilder(args);
 
-            //builder.Services.AddHostedService<ConsoleManager>();
             builder.Services.AddSingleton<ConsoleManager>();
             builder.Services.AddHostedService(x => x.GetRequiredService<ConsoleManager>()); //this makes sure we are using the same instance of ConsoleManager
             builder.Services.AddHostedService<NetworkListener>();
+            builder.Services.AddSingleton<ConnectionService>();
+            builder.Services.AddHostedService(x => x.GetRequiredService<ConnectionService>());
+            builder.Services.AddSingleton<ConfigurationService>();
             builder.Services.AddSingleton<Uploader>();
             builder.Services.AddSingleton<PlayerState>();
-            builder.Services.AddSingleton<ConfigurationService>();
             builder.Services.AddTransient<PowSolver>();
             builder.Services.AddSerilog();
             IHost host = builder.Build();
@@ -36,7 +37,6 @@ namespace AlbionDataSharp
                 .CreateLogger();
 
             var lifetime = host.Services.GetRequiredService<IHostApplicationLifetime>();
-            lifetime.ApplicationStopping.Register(() => host.Services.GetRequiredService<Uploader>().OnShutDown());
             AppDomain.CurrentDomain.ProcessExit += async (s, e) =>
             {
                 lifetime.StopApplication();
